eb14a3f7fa566979cd59219e8008181803cfbc01
Pranjal M <justmishra@outlook.com>
2020-05-11 01:27:32 +0530

    Wrote makeCacheMatrix Function

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inver <- NULL
+        setMat <- function(o){
+                x <<- o
+                inver <<- NULL
+        }
+        get <- function(){x}
+        setInv <- function(inverse) {inver <<- inverse}
+        getInv <- function() {inver}
+        list(set = set,
+             get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
